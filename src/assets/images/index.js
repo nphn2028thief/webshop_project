84cb9f4e595b0b6979a9bd0d74e960d1d88a983a
nphn2082thief <nphn2082thief@gmail.com>
2022-12-02 09:52:26 +0700

    Make filter for tablet and mobile (fix bugs ui) part 3

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -6,41 +6,41 @@ const images = {
     imageSlider2: require('~/assets/images/slider/slide_2.png'),
     imageSlider3: require('~/assets/images/slider/slide_3.png'),
 
-    product_01_image_01: require('~/assets/images/products/product-01 (1).png'),
-    product_01_image_02: require('~/assets/images/products/product-01 (2).png'),
+    product_01_image_01: require('~/assets/images/products/product-01 (1).jpg'),
+    product_01_image_02: require('~/assets/images/products/product-01 (2).jpg'),
 
-    product_02_image_01: require('~/assets/images/products/product-02 (1).png'),
-    product_02_image_02: require('~/assets/images/products/product-02 (2).png'),
+    product_02_image_01: require('~/assets/images/products/product-02 (1).jpg'),
+    product_02_image_02: require('~/assets/images/products/product-02 (2).jpg'),
 
-    product_03_image_01: require('~/assets/images/products/product-03 (1).png'),
-    product_03_image_02: require('~/assets/images/products/product-03 (2).png'),
+    product_03_image_01: require('~/assets/images/products/product-03 (1).jpg'),
+    product_03_image_02: require('~/assets/images/products/product-03 (2).jpg'),
 
-    product_04_image_01: require('~/assets/images/products/product-04 (1).png'),
-    product_04_image_02: require('~/assets/images/products/product-04 (2).png'),
+    product_04_image_01: require('~/assets/images/products/product-04 (1).jpg'),
+    product_04_image_02: require('~/assets/images/products/product-04 (2).jpg'),
 
-    product_05_image_01: require('~/assets/images/products/product-05 (1).png'),
-    product_05_image_02: require('~/assets/images/products/product-05 (2).png'),
+    product_05_image_01: require('~/assets/images/products/product-05 (1).jpg'),
+    product_05_image_02: require('~/assets/images/products/product-05 (2).jpg'),
 
-    product_06_image_01: require('~/assets/images/products/product-06 (1).png'),
-    product_06_image_02: require('~/assets/images/products/product-06 (2).png'),
+    product_06_image_01: require('~/assets/images/products/product-06 (1).jpg'),
+    product_06_image_02: require('~/assets/images/products/product-06 (2).jpg'),
 
-    product_07_image_01: require('~/assets/images/products/product-07 (1).png'),
-    product_07_image_02: require('~/assets/images/products/product-07 (2).png'),
+    product_07_image_01: require('~/assets/images/products/product-07 (1).jpg'),
+    product_07_image_02: require('~/assets/images/products/product-07 (2).jpg'),
 
-    product_08_image_01: require('~/assets/images/products/product-08 (1).png'),
-    product_08_image_02: require('~/assets/images/products/product-08 (2).png'),
+    product_08_image_01: require('~/assets/images/products/product-08 (1).jpg'),
+    product_08_image_02: require('~/assets/images/products/product-08 (2).jpg'),
 
-    product_09_image_01: require('~/assets/images/products/product-09 (1).png'),
-    product_09_image_02: require('~/assets/images/products/product-09 (2).png'),
+    product_09_image_01: require('~/assets/images/products/product-09 (1).jpg'),
+    product_09_image_02: require('~/assets/images/products/product-09 (2).jpg'),
 
-    product_10_image_01: require('~/assets/images/products/product-10 (1).png'),
-    product_10_image_02: require('~/assets/images/products/product-10 (2).png'),
+    product_10_image_01: require('~/assets/images/products/product-10 (1).jpg'),
+    product_10_image_02: require('~/assets/images/products/product-10 (2).jpg'),
 
-    product_11_image_01: require('~/assets/images/products/product-11 (1).png'),
-    product_11_image_02: require('~/assets/images/products/product-11 (2).png'),
+    product_11_image_01: require('~/assets/images/products/product-11 (1).jpg'),
+    product_11_image_02: require('~/assets/images/products/product-11 (2).jpg'),
 
-    product_12_image_01: require('~/assets/images/products/product-12 (1).png'),
-    product_12_image_02: require('~/assets/images/products/product-12 (2).png'),
+    product_12_image_01: require('~/assets/images/products/product-12 (1).jpg'),
+    product_12_image_02: require('~/assets/images/products/product-12 (2).jpg'),
 };
 
 export default images;
